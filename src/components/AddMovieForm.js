c8a2cc48bf81cbfc159043de086531c07df357e2
TeflonTrout <jtkazo@gmail.com>
2020-08-26 12:49:15 -0400

    Styling and home functionality

--- src/components/AddMovieForm.js
+++ src/components/AddMovieForm.js
@@ -1,24 +1,45 @@
-import React, {useState, useContext} from 'react'
+import React, {useState, useContext, useEffect} from 'react'
 import {MovieListContext} from '../MovieListContext'
+import axios from 'axios';
 
 const AddMovieForm = () => {
+    //API KEY FOR THEMOVIEDB.COM
+     const API_KEY = '08bd865f79a8f129e927b69c5220d722';
+
+    //SET STATE AND CONTEXT
     const [movieList, setMovieList] = useContext(MovieListContext)
     const [title, setTitle] = useState("");
     const [rating, setRating] = useState("0")
-    const [id, setId] = useState()
+    const [results, setResults] = useState([])
     
 
-
+    //UPDATED INPUT QUERY
     const updateTitle = (e) => {
         setTitle(e.target.value);
     }
 
+    //ADDS NEW MOVIE TO MOVIELIST ARRAY
     const addMovie = (e) => {
         e.preventDefault();
+
+        // axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${title}`)
+        //     .then((response) => {
+        //         console.log(response.data);
+        //         console.log(response.data.results);
+        //         setResults(response.data.results[0])
+        //     });
+
+
         setMovieList(movieList => [...movieList, {id: (Date.now()), title: title, rating: rating}])
-       console.log("movieAdded")
-       setTitle('')
+        console.log("added", title, "to the list")
+        console.log(results)
     }
+
+    //WHEN MOVIELIST IS UPDATED => CONSOLE LOG MOVIELIST
+    useEffect(() => {
+        console.log(movieList)
+    }, [movieList])
+    
     return (
         <div className="add-movie">
             <h1>ADD A MOVIE</h1>
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Movie from './Movie'
 import {MovieListContext} from "../MovieListContext";
+import axios from 'axios';
 
 const MovieList = () => {
     const [movieList, setMovieList] = useContext(MovieListContext);
@@ -13,24 +14,29 @@ const MovieList = () => {
         console.log(JSON.stringify(movieList));
         
 //Fetch POST to API
-        fetch('http://192.168.254.87:8080/api', {
-            method: 'POST',
-            mode: 'no-cors',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify([movieList])
-        }).then(response => {
-            if (response.status >= 200 && response.status < 300) {
-                return response;
-                console.log(response);
-            } else {
-                console.log('BIGLY ERROR', response)
-            }
-        }).catch(err => err);
-        console.log('POSTED', movieList);
-        }
+        axios.post('http://192.168.254.87:8080/api', {
+            firstName: 'Test',
+            lastName: 'Testing'
+        });
+
+        // fetch('http://192.168.254.87:8080/api', {
+        //     method: 'POST',
+        //     mode: 'no-cors',
+        //     headers: {
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify([movieList])
+        // }).then(response => {
+        //     if (response.status >= 200 && response.status < 300) {
+        //         return response;
+        //         console.log(response);
+        //     } else {
+        //         console.log('BIGLY ERROR', response)
+        //     }
+        // }).catch(err => err);
+        // console.log('POSTED', movieList);
+    }
 
     const handleRatingChange = (e, rating, id) => {
         setMovieList(movieList.map(movie => {
@@ -52,19 +58,19 @@ const MovieList = () => {
 
     return (
         <div className='movie-list'>
-            <form /*onSubmit={handleSubmit}*/>
+            <form onSubmit={handleSubmit}>
                 <div className='pity-point'>
-                    <p>Pity Point?</p>
+                    {/* <p>Pity Point?</p> */}
                 </div>
                 {movieList.map(movie => (
                     <div className='movie-component' key={movie.id}>
                         <h1>{movie.title}</h1>
-                        <input className='check-box' type="checkbox"/>
+                        {/* <input className='check-box' type="checkbox"/>
                         <input type='number'
                         placeholder={movie.rating}
                         onChange={e => handleRatingChange(e, (movie.rating), (movie.id))}
                         autoComplete="new-password"
-                        />
+                        /> */}
                         <button onClick={e => handleRemoveMovie(e, (movie.id))}><i class="fas fa-trash-alt"></i></button>
                     </div>
                 ))}
