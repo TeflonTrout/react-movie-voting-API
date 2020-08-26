a809df0325f61c94fc6ee500669200afac4c962b
TeflonTrout <jtkazo@gmail.com>
2020-08-26 12:59:01 -0400

    api post updated

--- src/components/VotingForm.js
+++ src/components/VotingForm.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { MovieListContext } from '../MovieListContext'
+import axios from 'axios';
 
 function VotingForm() {
     const [movieList, setMovieList] = useContext(MovieListContext);
@@ -15,9 +16,16 @@ function VotingForm() {
         console.log(movieList)
     };
 
+    const handleVotingSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://192.168.254.87:8080/api', {movieList});
+        console.log('You Voted')
+        console.log(movieList)
+    }
+
     return (
         <div className='voting-form'>
-            <form className='voting-form'>
+            <form className='voting-form' onSubmit={e => handleVotingSubmit(e)}>
                {movieList.map(movie=> (
                     <div className='voting-item' key={movie.id}>
                         <h1>{movie.title}</h1>
